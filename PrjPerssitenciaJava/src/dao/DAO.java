1a50dbe6240ff37d5dbd8f7fae1727cc25b4f8d0
Flavia Nunes Pino <flavitta007@gmail.com>
2019-02-07 16:19:45 -0200

    Classe Abstrata DAO criada

--- PrjPerssitenciaJava/src/dao/DAO.java
+++ PrjPerssitenciaJava/src/dao/DAO.java
@@ -1,5 +1,7 @@
 package dao;
 
 public abstract class DAO {
-
+  public abstract void Incluir(T elemento) throws Exception;
+  public T buscar(int id) throws Exception;
+  public Set<T> listar(int... param) throws Exception;
 }
